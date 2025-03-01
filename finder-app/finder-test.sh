0691d29bdeeb1b749bf1da0fca42b188391abd80
AhmedAlshehhi7 <AhmedAlshehhi7@github.com>
2025-03-01 20:28:09 +0000

    Update finder with latest changes for assignment-2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,65 +1,54 @@
 #!/bin/sh
-# finder-test.sh for Assignment 4 integration
-# This script tests the finder application.
-# It assumes the following executables are in the PATH:
-#   - finder.sh
-#   - writer
-# It expects configuration files to be located in /etc/finder-app/conf:
-#   - username.txt
-#   - assignment.txt (which should contain "assignment4")
-# The script writes the output of the finder command to /tmp/assignment4-result.txt.
 
 set -e
 set -u
 
 NUMFILES=10
-WRITESTR="AELD_IS_FUN"
-WRITEDIR="/tmp/aeld-data"
-
-# Read configuration from /etc/finder-app/conf
+WRITESTR=AELD_IS_FUN
+WRITEDIR=/tmp/aeld-data
 username=$(cat /etc/finder-app/conf/username.txt)
 
-if [ $# -ge 1 ]; then
-    NUMFILES=$1
-fi
-if [ $# -ge 2 ]; then
-    WRITESTR=$2
-fi
-if [ $# -ge 3 ]; then
-    WRITEDIR=$3
+if [ $# -lt 3 ]
+then
+	echo "Using default value ${WRITESTR} for string to write"
+	if [ $# -lt 1 ]
+	then
+		echo "Using default value ${NUMFILES} for number of files to write"
+	else
+		NUMFILES=$1
+	fi	
+else
+	NUMFILES=$1
+	WRITESTR=$2
+	WRITEDIR=/tmp/aeld-data/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
 
 echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
+
 rm -rf "${WRITEDIR}"
 
-assignment=$(cat /etc/finder-app/conf/assignment.txt)
-if [ "$assignment" != "assignment4" ]; then
-    mkdir -p "$WRITEDIR"
-    if [ -d "$WRITEDIR" ]; then
-        echo "$WRITEDIR created"
-    else
-        echo "Failed to create $WRITEDIR"
-        exit 1
-    fi
+# create $WRITEDIR if not assignment1
+assignment=`cat /etc/finder-app/conf/assignment.txt`
+
+if [ $assignment != 'assignment1' ]
+then
+	mkdir -p "$WRITEDIR"
+
+	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
+	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
+	if [ -d "$WRITEDIR" ]
+	then
+		echo "$WRITEDIR created"
+	else
+		exit 1
+	fi
 fi
 
-for i in $(seq 1 $NUMFILES); do
-    writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+for i in $( seq 1 $NUMFILES)
+do
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
-rm -rf "${WRITEDIR}"
-
-echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt
-
-echo "${OUTPUTSTRING}" | grep "${MATCHSTR}"
-if [ $? -eq 0 ]; then
-    echo "success"
-    exit 0
-else
-    echo "failed: expected ${MATCHSTR} in ${OUTPUTSTRING}"
-    exit 1
-fi
-
+finder.sh "$WRITEDIR" "$WRITESTR" >/tmp/assignment4-result.txt
