c127069810b1c2fb3220b4ebfa29ec4e6eb3a676
AhmedAlshehhi7 <AhmedAlshehhi7@github.com>
2025-03-02 09:05:33 +0000

    Update finder scripts and test scripts for assignment-2

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
-# finder.sh filesdir searchstr
+# finder.sh: search for a string in all files within a directory
 
-if [[ $# != 2 ]]; then
-	echo >&2 "Usage: $0 filesdir searchstr"
-	exit 1
+if [ "$#" -ne 2 ]; then
+    echo >&2 "Usage: $0 filesdir searchstr"
+    exit 1
 fi
 
 filesdir=$1
 searchstr=$2
-if [[ ! -d $filesdir ]]; then
-	echo >&2 "$0: $filesdir is not a directory"
-	exit 1
+
+if [ ! -d "$filesdir" ]; then
+    echo >&2 "$0: $filesdir is not a directory"
+    exit 1
 fi
 
 files=0
 lines=0
-for f in $filesdir/**; do
-	if [[ ! -f $f ]]; then
-		continue
-	fi
-	files=$(( $files + 1 ))
-	lines=$(( $lines + $(grep "${searchstr}" $f | wc -l) ))
+
+# Use find to iterate over files recursively
+for f in $(find "$filesdir" -type f); do
+    files=$((files + 1))
+    count=$(grep "$searchstr" "$f" | wc -l)
+    lines=$((lines + count))
 done
+
 echo "The number of files are $files and the number of matching lines are $lines"
 exit 0
+
